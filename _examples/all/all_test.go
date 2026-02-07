96c703b269399be6c5ad3363a225a56015c99f26
cosasce <cosasce@gmail.com>
2026-02-07 05:58:00 +0000

    Remove pprof

--- _examples/all/all_test.go
+++ _examples/all/all_test.go
@@ -1,8 +1,6 @@
 package all
 
 import (
-	"net/http"
-	_ "net/http/pprof"
 	"testing"
 )
 
@@ -11,12 +9,6 @@ func TestAll(t *testing.T) {
 }
 
 func BenchmarkAll(b *testing.B) {
-	go func() {
-		err := http.ListenAndServe("localhost:9090", nil)
-		if err != nil {
-			b.Log(err.Error())
-		}
-	}()
 	for i := 0; i < b.N; i++ {
 		All()
 	}
